97c5cd9ad0580fca5e597c100a81b3e743da30c1
Jin Li <lijin@Tony.local>
2017-06-19 14:39:52 +0200

    get prior function and posterior of Gaussian process

--- GP_regression.py
+++ GP_regression.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-np.set_printoptions(precision=10, suppress=True)
+np.set_printoptions(precision=3, suppress=True)
 
 f = lambda x: np.sin(0.9*x).flatten()
 
 # RBF kernel
 def RBF_kernel(a,b,kernel_parameter):
     sqdist = np.sum(a**2,1).reshape(-1, 1) - 2*np.dot(a, b.T) + np.sum(b**2,1).reshape(1, -1)
-    return np.exp(-.5 * (1 / kernel_parameter) * sqdist)
+    dis_mean = 1
+    return dis_mean * np.exp(-.5 * (1 / kernel_parameter) * sqdist)
 
-N = 10     # number of training points
+N = 5     # number of training points
 n = 50     # number of test points
 s = 0.0005  # noise variance and zero mean for noise
 num_fun = 10 # number of prior function
 kernel_parameter = 1
+kernel_stand_deiv = 1
 
 # Sample N input points of noisy version of the function evaluated at these points
 t = np.linspace(-5, 5, N)
@@ -25,16 +27,19 @@ plt.plot(X_train, y_train, 'ro')
 # points we're going to make predictions at.
 X_test = np.linspace(-5, 5, n).reshape(-1, 1)
 # sampling from a multivariate Gaussian for prior function
-mu = np.zeros((n, 1)) # zero mean for multivariate Gaussian
+mu_prior = np.zeros((n, 1)) # zero mean for multivariate Gaussian
 K = RBF_kernel(X_test, X_test, kernel_parameter) # covariance matrix for prior function
 B = np.linalg.cholesky(K + 1e-6 * np.eye(n))
-f_prior = mu + np.dot(B, np.random.normal(size=(n, num_fun)))
-print f_prior.shape
+f_prior = mu_prior + np.dot(B, np.random.normal(size=(n, num_fun)))
 
 # plot prior function
+#plt.figure(1)
+plt.subplot(2,2,1)
+#plt.clf()
+plt.fill_between(X_test.flat, 0 - kernel_stand_deiv, 0 + kernel_stand_deiv, color="#dddddd")
 plt.plot(X_test, f_prior)
+plt.title('samples from the GP prior')
 plt.axis([-5, 5, -3, 3])
-#plt.show()
 
 # make prediction for test data by posterior
 K_train = RBF_kernel(X_train, X_train, kernel_parameter)
@@ -42,25 +47,36 @@ L = np.linalg.cholesky(K_train + s * np.eye(N))
 m = np.linalg.solve(L, y_train)
 alpha = np.linalg.solve(L.T, m)
 K_s = RBF_kernel(X_train, X_test, kernel_parameter)
-mu = np.dot(K_s.T, alpha)
-#cov_matrix = RBF_kernel(X_test)
-#print mu
+# compute mean of test points for posterior
+mu_post = np.dot(K_s.T, alpha)
 K_ss = RBF_kernel(X_test, X_test, kernel_parameter)
 v = np.linalg.solve(L, K_s)
-cov_matrix = K_ss - np.dot(v.T, v)
-# sample from posterior function
-L_ = np.linalg.cholesky(cov_matrix + 1e-6 * np.eye(n))
-f_post = mu.reshape(-1,1) + np.dot(L_, np.random.normal(size=(n, num_fun)))
-plt.figure(1)
-plt.clf()
-plt.plot(X_test, f_post)
 
-plt.figure(2)
-plt.clf()
+# compute variance for test points
+var_test = np.diag(K_ss) - np.sum(v**2, axis=0)
+stand_devi = np.sqrt(var_test)
+
+# sample from test points
+L_ = np.linalg.cholesky(K_ss + 1e-6 * np.eye(n) - np.dot(v.T, v))
+f_post = mu_post.reshape(-1, 1) + np.dot(L_, np.random.normal(size=(n, num_fun)))
+plt.axis([-5, 5, -3, 3])
+
+#plt.figure(2)
+plt.subplot(2,2,2)
+#plt.clf()
+plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
+plt.plot(X_test, f_post)
+plt.plot(X_test, mu_post, 'r--', lw=2)
+plt.title('samples from the GP posterior')
+plt.axis([-5, 5, -3, 3])
+
+#plt.figure(3)
+plt.subplot(2,2,3)
+#plt.clf()
 plt.plot(X_train, y_train, 'r+', ms=20)
 plt.plot(X_test, f(X_test), 'b-')
-plt.gca().fill_between(X_test.flat, mu-3*s, mu+3*s, color="#dddddd")
-plt.plot(X_test, mu, 'r--', lw=2)
+plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
+plt.plot(X_test, mu_post, 'r--', lw=2)
 #plt.savefig('predictive.png', bbox_inches='tight')
 plt.title('Mean predictions plus 3 st.deviations')
 plt.axis([-5, 5, -3, 3])
