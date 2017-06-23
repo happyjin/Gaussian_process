8727bc60b1a7dd8e6e783efbf56051883b6dc248
Jin Li <engineer.tony.lee@gmail.com>
2017-06-23 11:35:48 +0200

    get prior function and posterior of Gaussian process

---
+++ GP_binary_classification.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from scipy.special import expit # logistic function
+from GP_regression import RBF_kernel, f_prior
+np.set_printoptions(precision=3, suppress=True, threshold=np.nan)
+
+# generate dataset
+def dataset_generator():
+    X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
+                               random_state=1, n_clusters_per_class=1)
+    rng = np.random.RandomState(2)
+    X += 2 * rng.uniform(size=X.shape)
+    linearly_separable = (X, y)
+
+    datasets = [make_moons(noise=0.3, random_state=0),
+                make_circles(noise=0.2, factor=0.5, random_state=1),
+                linearly_separable
+                ]
+
+    X, y = datasets[0]
+    y[y == 0] = -1
+    X = StandardScaler().fit_transform(X)
+    return X, y
+
+
+# log_likelihood function
+def log_likelihood(z):
+    return -np.log(1 + np.exp(z))
+
+
+# first derivative of log_likelihood function
+def deriv_log_likelihood(y, z):
+    t = (y + 1) / 2
+    return t - expit(z)
+
+
+# second derivative of log_likelihood function
+def sec_deriv_log_likelihood(z):
+    return -expit(z) * (1 - expit(z))
+
+
+# Newton method function
+def newton_method(K, y, z):
+    num_ele = y.size
+    first_deri = np.zeros((num_ele, num_ele))
+    W = np.zeros((num_ele, num_ele))
+
+    np.fill_diagonal(first_deri, deriv_log_likelihood(y, z).flatten())
+    np.fill_diagonal(W, sec_deriv_log_likelihood(z))
+
+
+
+
+if __name__ == "__main__":
+
+    # generate dataset
+    X, y = dataset_generator()
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.4, random_state=42)
+    num_train = y_train.size
+
+    # hyper-parameters
+    num_funs = 1 # number of GP prior functions
+    # number of sampling arbitrary points for GP prior. For simplicity we assume num_sampling is equal to num_train
+    num_sampling = num_train
+    kernel_parameter = 1
+
+    # plot dataset scatter at first
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    plt.subplot(2,3,1)
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+    plt.title('data points')
+
+    # compute covariance matrix K under RBF_kernel
+    K_train = RBF_kernel(X_train, X_train, kernel_parameter)
+
+    # sampling points for GP prior function
+    x1_min = np.min(X[:,0])
+    x1_max = np.max(X[:,0])
+    x2_min = np.min(X[:,1])
+    x2_max = np.max(X[:,1])
+    X1_sampling = np.linspace(x1_min, x1_max, num_sampling).reshape(-1, 1)
+    X2_sampling = np.linspace(x2_min, x2_max, num_sampling).reshape(-1, 1)
+    X_sampling = np.concatenate((X1_sampling, X2_sampling), axis=1)
+
+    # sampling GP prior sampling GP prior f for likelihood
+    mu_prior = np.zeros((num_sampling, 1))
+    num_sampling = X_sampling.shape[0]
+    f_prior = f_prior(X_sampling, mu_prior, kernel_parameter, num_sampling, num_funs)
+    print 'shape of prior function:' + `f_prior.shape`
+    plt.subplot(2,3,2)
+    #plt.clf()
+    plt.plot(np.sort(X_sampling[:,0]), f_prior)
+    plt.title('latent function-prior')
+
+    # compute probability of GP prior under y=+1 compute likelihood function p(y|f)
+    y = +1
+    prob_pred_prior = expit(y * f_prior)
+    plt.subplot(2,3,3)
+    #plt.clf()
+    plt.plot(np.sort(X_sampling[:,0]), prob_pred_prior)
+    plt.title('probability for latent function-prior')
+    #plt.show()
+    # deterministic function
+    #print y_pred >0.5
+
+    # compute likelihood function p(y|f), we fix y(training label) and vary GP prior function f
+    y_train = y_train.reshape(-1, 1)
+    z = y_train * f_prior
+    prob_likelihood = expit(z)
+    log_likeli = log_likelihood(z)
+    deriv_log_likeli = deriv_log_likelihood(y_train, z)
+    sec_deriv_log_likeli = sec_deriv_log_likelihood(z)
+
+    # log likelihood and its derivatives figure
+    plt.subplot(2,3,4)
+    #plt.clf()
+    i = 0
+    plt.plot(z[:,i], log_likeli[:,i], 'bo', label='log likelihood')
+    plt.plot(z[:,i], deriv_log_likeli[:,i], 'ro', label='1st derivative')
+    plt.plot(z[:,i], sec_deriv_log_likeli[:,i], 'go', label='2nd derivative')
+    plt.legend()
+    #plt.show()
+
+    # newton iteration
+    newton_method(K_train, y_train, z)
--- GP_regression.py
+++ GP_regression.py
@@ -2,82 +2,90 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=3, suppress=True)
 
-f = lambda x: np.sin(0.9*x).flatten()
-
 # RBF kernel
 def RBF_kernel(a,b,kernel_parameter):
-    sqdist = np.sum(a**2,1).reshape(-1, 1) - 2*np.dot(a, b.T) + np.sum(b**2,1).reshape(1, -1)
+    # loop vectorization
+    sqdist = ((a[:, :, None] - b[:, :, None].T) ** 2).sum(1)
     dis_mean = 1
     return dis_mean * np.exp(-.5 * (1 / kernel_parameter) * sqdist)
 
-N = 5           # number of training points
-n = 50          # number of test points
-s = 0.0005      # noise variance and zero mean for noise
-num_fun = 10    # number of prior function
-kernel_parameter = 1
-kernel_stand_deiv = 1 # standard deviation for kernel
+# f_prior function
+def f_prior(X_test, mu_prior, kernel_parameter, num_test, num_fun):
+    kernel = RBF_kernel(X_test, X_test, kernel_parameter)  # covariance matrix for prior function
+    B = np.linalg.cholesky(kernel + 1e-6 * np.eye(num_test))
+    f_prior = mu_prior + np.dot(B, np.random.normal(size=(num_test, num_fun)))
+    return f_prior
 
-# Sample N input points of noisy version of the function evaluated at these points
-t = np.linspace(-5, 5, N)
-X_train = np.random.uniform(-5, 5, size=(N,1))
-y_train = f(X_train) + np.sqrt(s)*np.random.randn(N)
-plt.plot(X_train, y_train, 'ro')
 
-# points we're going to make predictions at.
-X_test = np.linspace(-5, 5, n).reshape(-1, 1)
-# sampling from a multivariate Gaussian for prior function
-mu_prior = np.zeros((n, 1)) # zero mean for multivariate Gaussian
-K = RBF_kernel(X_test, X_test, kernel_parameter) # covariance matrix for prior function
-B = np.linalg.cholesky(K + 1e-6 * np.eye(n))
-f_prior = mu_prior + np.dot(B, np.random.normal(size=(n, num_fun)))
+if __name__ == "__main__":
 
-# plot prior function
-#plt.figure(1)
-plt.subplot(2,2,1)
-#plt.clf()
-plt.fill_between(X_test.flat, 0 - kernel_stand_deiv, 0 + kernel_stand_deiv, color="#dddddd")
-plt.plot(X_test, f_prior)
-plt.title('samples from the GP prior')
-plt.axis([-5, 5, -3, 3])
+    N = 5           # number of training points
+    n = 100          # number of test points
+    s = 0.0005      # noise variance and zero mean for noise
+    num_fun = 10    # number of prior function
+    kernel_parameter = 1
+    kernel_stand_deiv = 1 # standard deviation for kernel
 
-# make prediction for test data by posterior
-K_train = RBF_kernel(X_train, X_train, kernel_parameter)
-L = np.linalg.cholesky(K_train + s * np.eye(N))
-m = np.linalg.solve(L, y_train)
-alpha = np.linalg.solve(L.T, m)
-K_s = RBF_kernel(X_train, X_test, kernel_parameter)
-# compute mean of test points for posterior
-mu_post = np.dot(K_s.T, alpha)
-K_ss = RBF_kernel(X_test, X_test, kernel_parameter)
-v = np.linalg.solve(L, K_s)
+    # Sample N input points of noisy version of the function evaluated at these points
+    f = lambda x: np.sin(0.9 * x).flatten()
+    t = np.linspace(-5, 5, N)
+    X_train = np.random.uniform(-5, 5, size=(N,1))
+    y_train = f(X_train) + np.sqrt(s)*np.random.randn(N)
+    plt.plot(X_train, y_train, 'ro')
 
-# compute variance for test points
-var_test = np.diag(K_ss) - np.sum(v**2, axis=0)
-stand_devi = np.sqrt(var_test)
+    # points we're going to make predictions at.
+    X_test = np.linspace(-5, 5, n).reshape(-1, 1)
+    # sampling from a multivariate Gaussian for prior function
+    mu_prior = np.zeros((n, 1)) # zero mean for multivariate Gaussian
+    f_prior = f_prior(X_test, mu_prior, kernel_parameter, n, num_fun)
 
-# sample from test points
-L_ = np.linalg.cholesky(K_ss + 1e-6 * np.eye(n) - np.dot(v.T, v))
-f_post = mu_post.reshape(-1, 1) + np.dot(L_, np.random.normal(size=(n, num_fun)))
-plt.axis([-5, 5, -3, 3])
+    # plot prior function
+    #plt.figure(1)
+    plt.subplot(2,2,1)
+    #plt.clf()
+    plt.fill_between(X_test.flat, 0 - kernel_stand_deiv, 0 + kernel_stand_deiv, color="#dddddd")
+    plt.plot(X_test, f_prior)
+    plt.title('samples from the GP prior')
+    plt.axis([-5, 5, -3, 3])
 
-#plt.figure(2)
-plt.subplot(2,2,2)
-#plt.clf()
-plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
-plt.plot(X_test, f_post)
-plt.plot(X_test, mu_post, 'r--', lw=2)
-plt.title('samples from the GP posterior')
-plt.axis([-5, 5, -3, 3])
+    # make prediction for test data by posterior
+    K_train = RBF_kernel(X_train, X_train, kernel_parameter)
+    L = np.linalg.cholesky(K_train + s * np.eye(N))
+    m = np.linalg.solve(L, y_train)
+    alpha = np.linalg.solve(L.T, m)
+    K_s = RBF_kernel(X_train, X_test, kernel_parameter)
+    # compute mean of test points for posterior
+    mu_post = np.dot(K_s.T, alpha)
+    K_ss = RBF_kernel(X_test, X_test, kernel_parameter)
+    v = np.linalg.solve(L, K_s)
 
-#plt.figure(3)
-plt.subplot(2,2,3)
-#plt.clf()
-plt.plot(X_train, y_train, 'r+', ms=20)
-plt.plot(X_test, f(X_test), 'b-')
-plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
-plt.plot(X_test, mu_post, 'r--', lw=2)
-#plt.savefig('predictive.png', bbox_inches='tight')
-plt.title('Mean predictions plus 3 st.deviations')
-plt.axis([-5, 5, -3, 3])
+    # compute variance for test points
+    var_test = np.diag(K_ss) - np.sum(v**2, axis=0)
+    stand_devi = np.sqrt(var_test)
 
-plt.show()
+    # sample from test points
+    L_ = np.linalg.cholesky(K_ss + 1e-6 * np.eye(n) - np.dot(v.T, v))
+    f_post = mu_post.reshape(-1, 1) + np.dot(L_, np.random.normal(size=(n, num_fun)))
+    plt.axis([-5, 5, -3, 3])
+
+    #plt.figure(2)
+    plt.subplot(2,2,2)
+    #plt.clf()
+    plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
+    plt.plot(X_test, f_post)
+    plt.plot(X_test, mu_post, 'r--', lw=2)
+    plt.title('samples from the GP posterior')
+    plt.axis([-5, 5, -3, 3])
+
+    #plt.figure(3)
+    plt.subplot(2,2,3)
+    #plt.clf()
+    plt.plot(X_train, y_train, 'r+', ms=20)
+    plt.plot(X_test, f(X_test), 'b-')
+    plt.gca().fill_between(X_test.flat, mu_post - 3 * stand_devi, mu_post + 3 * stand_devi, color="#dddddd")
+    plt.plot(X_test, mu_post, 'r--', lw=2)
+    #plt.savefig('predictive.png', bbox_inches='tight')
+    plt.title('Mean predictions plus 3 st.deviations')
+    plt.axis([-5, 5, -3, 3])
+
+    plt.show()
