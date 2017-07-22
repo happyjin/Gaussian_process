edcfb8f3552ec65a0f22ac9a7fb37bc2f2eed5a7
Jin Li <engineer.tony.lee@gmail.com>
2017-07-22 11:53:20 +0200

    fix random sampling

--- tune_hyperparms_regression.py
+++ tune_hyperparms_regression.py
@@ -3,6 +3,7 @@ from GP_regression import dataset_generator, RBF_kernel, plot_posterior, plot_tr
 from scipy.stats import norm
 import random
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=3, suppress=True)
 
@@ -83,6 +84,7 @@ def bayesian_opt(X_train, X_test, y_train):
     #plot_BO(X_train, y_train, X_test, f_post_fun, mu_post, stand_devi)
     return mu_post, stand_devi, f_post_fun
 
+
 def tune_hyperparms_first(X_train, X_test, y_train, num_fun, sigma, l):
     """
     maximize log marginal likelihood using gradient ascent
@@ -140,7 +142,8 @@ def tune_hyperparms_first(X_train, X_test, y_train, num_fun, sigma, l):
     plt.axis([-5, 5, -3, 3])
     return mu_post, stand_devi, f_post_fun, optimal_likelihood
 
-def EI(params, means, stand_devi, parms_done, y):
+
+def PI(params, means, stand_devi, parms_done, y):
     s = 0.0005  # small value
     stop_threshold = 0.001
     max_mean = np.max(y)
@@ -168,15 +171,25 @@ def EI(params, means, stand_devi, parms_done, y):
 
 
 def UCB(params, means, stand_devi, parms_done, y):
-    kappa = 0.5
+    num_parms = len(parms_done)
+    kappa = 7
+
     objective = means + kappa * stand_devi
     indices = np.where(objective == np.max(objective))[0]
     indices = np.asarray(indices)
+    next_point = params[indices[0]]
+    # if the next selected point is as same as the last one, then stop it
+    if parms_done[num_parms - 1] == next_point:
+        return True
+    return next_point
 
+
+def TS(params, means, stand_devi, parms_done, y):
+    bayesian_opt(X_train, X_test, y_train)
 
 
 def acquisition_fun(params, means, stand_devi, parms_done, y):
-    next_point = EI(params, means, stand_devi, parms_done, y)
+    next_point = PI(params, means, stand_devi, parms_done, y)
     #next_point = UCB(params, means, stand_devi, parms_done, y)
     return next_point
 
@@ -204,13 +217,41 @@ def posterior_prediction(X_train, X_test, y_train, sigma, l):
     log_marg_likelihood = -.5 * np.dot(y_train.T, alpha) - np.diagonal(L).sum(0) - n / 2 * np.log(2 * np.pi)
     return log_marg_likelihood
 
+
+def overlap(a, b):
+    """
+    return the indices in a that overlap with b, also returns
+    the corresponding index in b only works if both a and b are unique!
+    This is not very efficient but it works
+    :param a: the first numpy array
+    :param b: the second numpy array
+    :return: indices for both numpy arrays
+    """
+    bool_a = np.in1d(a,b)
+    ind_a = np.arange(len(a))
+    ind_a = ind_a[bool_a]
+    ind_b = np.array([np.argwhere(b == a[x]) for x in ind_a]).flatten()
+    return ind_a,ind_b
+
+def random_gen_test_parms(n, parms_done):
+    num_gen = n + len(parms_done) + 10
+    test_parms = np.linspace(0.01, 5, num_gen)
+    # remove duplicates between training and test points and then sample from non-duplicates test points
+    ind_done, ind_sample = overlap(parms_done, test_parms)
+    test_parms = np.delete(test_parms, ind_sample)
+    test_parms_sampled = random.sample(test_parms, n)
+    test_parms_sampled = np.asarray(test_parms_sampled)
+    sample_sort = np.sort(test_parms_sampled)
+    return sample_sort.reshape(-1, 1)
+
+
 def tune_hyperparms_second(X_train, X_test, y_train, num_fun, sigma, l):
     s = 0  # noise variance and zero mean for noise
-    log_marg_likelihood = np.zeros(len(l))
-    itrations = len(l)
-    l_test = np.linspace(0.01, 5, n).reshape(-1, 1)
+    n = 100 # number of test points
 
-    for k in range(300):
+    for k in range(10):
+        # randomly generate test hyperparms except for hyperparms that have already choosed
+        l_test = random_gen_test_parms(n, l)
         log_marg_likelihood = np.zeros(len(l))
         for i in range(len(l)):
             log_marg_likelihood[i] = posterior_prediction(X_train, X_test, y_train, sigma, l[i])
@@ -226,18 +267,19 @@ def tune_hyperparms_second(X_train, X_test, y_train, num_fun, sigma, l):
             break
         l = np.append(l, next_point)
         max_index = np.where(log_marg_likelihood == np.max(log_marg_likelihood))[0]
-        #print "maximum parameters is:" + `l[max_index][0]`
-        #print ""
 
     log_marg_likelihood = np.zeros(len(l))
     for i in range(len(l)):
         log_marg_likelihood[i] = posterior_prediction(X_train, X_test, y_train, sigma, l[i])
 
+    print "it takes " + `k` + " iterations to get the optimal!"
+    print "optimal lenghscalar is:" + `l[max_index][0]`
     print "maximum likelihood is:" + `np.max(log_marg_likelihood)`
     # Bayesian optimization for hyperparameters
+    l_test = random_gen_test_parms(n, l)
     mu_post, stand_devi, f_post_fun = bayesian_opt(l.reshape(-1, 1), l_test, log_marg_likelihood)
     # plot Bayesian optimization
-    #plot_BO(l.reshape(-1,1), log_marg_likelihood, l_test, f_post_fun, mu_post, stand_devi)
+    plot_BO(l.reshape(-1,1), log_marg_likelihood, l_test, f_post_fun, mu_post, stand_devi)
     return np.max(log_marg_likelihood)
 
 
@@ -296,7 +338,5 @@ if __name__ == "__main__":
     print ""
     print "------ error rate ------"
     print("The error rate of optimal likelihood between two methods is: %.3f%%" % error_rate)
-
-
 
     #plt.show()
